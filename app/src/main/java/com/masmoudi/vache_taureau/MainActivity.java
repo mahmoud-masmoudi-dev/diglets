8f7bd01e74951254aa1ac94cb588547de5dccd85
Mahmoud Masmoudi <masmoudi.mahmoud@yahoo.fr>
2018-01-29 23:13:29 +0100

    First commit

---
+++ app/src/main/java/com/masmoudi/vache_taureau/MainActivity.java
@@ -0,0 +1,22 @@
+package com.masmoudi.vache_taureau;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view) {
+        TextView textView = (TextView)view;
+        String text = textView.getText().toString();
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
