204555d15cdd4223c06b59ea2253f92051a019ad
Mahmoud Masmoudi <masmoudi.mahmoud@yahoo.fr>
2018-02-03 22:31:57 +0100

    lettersBackspace activated

--- app/src/main/java/com/masmoudi/vache_taureau/MainActivity.java
+++ app/src/main/java/com/masmoudi/vache_taureau/MainActivity.java
@@ -8,12 +8,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void buttonClicked(View view) {
-        TextView textView = (TextView)view;
-        String text = textView.getText().toString();
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-    }
+    public TextView lettersAnswer;
 
+    public void lettersBackspaceClicked(View view) {
+        TextView textView = (TextView)view;
+        lettersAnswer = (TextView)findViewById(R.id.lettersAnswer);
+        String lettersAnswerText = lettersAnswer.getText().toString();
+        Toast.makeText(this, lettersAnswerText, Toast.LENGTH_SHORT).show();
+        if(!lettersAnswerText.isEmpty()) {
+            lettersAnswerText = lettersAnswerText.substring(0, lettersAnswerText.length() - 1);
+            lettersAnswer.setText(lettersAnswerText);
+        }
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
