c8c4566f7a3f4f3d1ef38e78f7def918a1130181
Mahmoud Masmoudi <mahmoud.masmoudi.dev@gmail.com>
2018-06-30 17:18:04 +0200

    Optimized imports

--- app/src/main/java/com/m_square/diglets/GameActivity.java
+++ app/src/main/java/com/m_square/diglets/GameActivity.java
@@ -2,13 +2,11 @@ package com.m_square.diglets;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
